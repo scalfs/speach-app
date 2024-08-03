2ae223a7c81039fd4547040533736e8a61162398
Vitor Escalfoni <vitor.hme@gmail.com>
2024-08-03 08:30:52 -0300

    :card_file_box: raw: updating voice ids

--- app/modules/eleven-labs/voices.ts
+++ app/modules/eleven-labs/voices.ts
@@ -1,25 +1,6 @@
 // TODO: move this to DB and associate voices to subscriptions/roles
-// This must be used when the EL_API_KEY from junior@pontocomaudio.net is active
-// export const AVAILABLE_VOICES = [
-//   'j08XqtjVs3KAr6mTIyWn',
-//   '2yRlrIiSvA1uvpfOs0K2',
-//   'lzG2jXkwmMBrwpyCrjTz',
-//   'h39L8YcPiu1V4eJdPRnL',
-//   '8QBBPiZao7z6fGxZfGF5',
-//   'k93ajjKq6xjpEIKUHsTJ',
-//   'g4013ncLlEJVq9y1so0x',
-//   'jx2mjueU4KnBPFYnKwbY',
-//   'CzCBufhIynIiUHjKZg84',
-//   'KzATwWK5DIWSzflGduat',
-//   'ByMI9LrFnrtnktXpNMzW',
-//   'qopOiYLpqehP85YmewGT',
-//   'k0Yl4TLZ4Qk7kX0sld99',
-//   'plwnhEgJFqYTzxwqt0R4',
-//   'Kfk31SD53YEK2JRARFO5',
-// ]
-
-// This must be used when the EL_API_KEY from speach01@pontocomaudio.net is active
 export const AVAILABLE_VOICES = [
+  // speach01@pontocomaudio.net
   'sA08OlI57z554552XXjX',
   's1jU0O6YsnX1rolenQ2A',
   'o3RJix088ugyZcwwYM3N',
@@ -35,11 +16,46 @@ export const AVAILABLE_VOICES = [
   '78kCnUrLe93dge5r4WLz',
   '70v6Tw9xxKlAxwX6exJX',
   '3M0mGfcPpXDcCvaGMB09',
+  // speach02@pontocomaudio.net
+  'GEAOOSvHHcqfphelQWrc',
+  'HLqJ0A0lvE7wQKabLTqf',
+  'MRN0BwIfP7DMNzXRUvtn',
+  '99jlxGHWKu178be7axXQ',
+  'L4eFA4NZRZXOOBXQfena',
+  'WtKRL8Y8Bh4oqaRLowky',
+  'GxGTGX8HJYUj0OxfOpiC',
+  'ibL9zzF9gQFzuauNevpB',
+  'Gw64dPxOgEIUn9ZPgPK9',
+  'qPMn31xkY1PQayr5n5N1',
+  '6yb1kH65UbAHssTo7e0z',
+  'tnhVCnroNZuLZa8PMbdS',
+  'vnbpfDufGMDPGeF7Xy88',
+  'uqhlwkQJuX6NA1Vox5KN',
+  'e3E7F7MSXzL91fyczk2S',
 ]
 
 export const PERSONAL_VOICES = {
-  'kseteimagerMKT@gmail.com': ['1nu83aLfBdCSeBijsrqx'],
-  'ronibage@gmail.com': ['TSXt20a5VzGITXSg9P9w', 'Id8EJ0S5cTNlkQtT6PFH'],
+  'kseteimagerMKT@gmail.com': [
+    // speach01@pontocomaudio.net
+    '1nu83aLfBdCSeBijsrqx',
+    // speach02@pontocomaudio.net
+    'ch5ObWzLcwFNv7hTKjvb',
+  ],
+  'ronibage@gmail.com': [
+    // speach01@pontocomaudio.net
+    'TSXt20a5VzGITXSg9P9w',
+    'Id8EJ0S5cTNlkQtT6PFH',
+    // speach02@pontocomaudio.net
+    'xtfncMXhicwOj7VeKHGD',
+    'g454sDXrbK3uZd1Zf7xC',
+    'nfxPSYrZs2ChEdKljcX8',
+    'eOuRsVMWfGIZ8JqVgBrl',
+  ],
+  'tsantos@redegzeta.com.br': [
+    // speach02@pontocomaudio.net
+    'RW8bhMYNUNiU7FmQOGEu',
+    'q53FNWaXSIyNftWD7AYg',
+  ],
 } as Record<string, string[]>
 
 // US voices to be used in the future
